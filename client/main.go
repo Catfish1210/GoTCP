0c876392b681e19b29f86afeb6918e779b4e5708
Catfish1210 <chris.laks@gmail.com>
2023-06-27 20:40:11 +0300

    Basic TCP connection

---
+++ client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	connection, err := net.Dial("tcp", "localhost:8080")
+	logFatal(err)
+
+	defer connection.Close()
+
+	fmt.Println("Enter your username: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	username, err := reader.ReadString('\n')
+	logFatal(err)
+
+	username = strings.Trim(username, " \r\n")
+
+	welcomeMsg := fmt.Sprintf("Welcome %s, to the GoTCP Chat \n", username)
+
+	connection.Write([]byte(welcomeMsg))
+}
---
+++ server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var (
+	openConnections = make(map[net.Conn]bool)
+	newConnection   = make(chan net.Conn)
+	deadConnection  = make(chan net.Conn)
+)
+
+func main() {
+	fmt.Println("Test server")
+
+	ln, err := net.Listen("tcp", ":8080")
+	logFatal(err)
+
+	defer ln.Close()
+
+	go func() {
+		for {
+
+			conn, err := ln.Accept()
+			logFatal(err)
+
+			openConnections[conn] = true
+			newConnection <- conn
+
+		}
+	}()
+
+	connection := <-newConnection
+
+	reader := bufio.NewReader(connection)
+
+	message, err := reader.ReadString('\n')
+	logFatal(err)
+
+	fmt.Println(message)
+}
